9076c1caea7b769c9ba1f5e3e3c027d13339df54
erictheballer <basketballfreak43@gmail.com>
2014-04-25 16:56:21 -0700

    pythonroom update

--- draw.py
+++ draw.py
@@ -2,15 +2,15 @@
 
 import turtle
 
-n = 
-length = 125
-angle = 180 - 180 * (n-2) / n
+n = 150
+length = 100
+angle = 150
 numbers = range(0, n)
 
 tyrone = turtle.Turtle()
 
 for number in numbers:
-	tyrone.color("#FF04C1")
+	
 	tyrone.forward(length)
 	tyrone.left(angle)
 	
