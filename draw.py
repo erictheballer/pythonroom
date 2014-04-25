d7d7a28394f4c08ec859239d9dd33df360382b1b
erictheballer <basketballfreak43@gmail.com>
2014-04-25 16:17:57 -0700

    pythonroom update

--- draw.py
+++ draw.py
@@ -4,9 +4,11 @@ import turtle
 
 length = 75
 angle = 60
+count = 0
 
 tyrone = turtle.Turtle()
 
-
-tyrone.forward(length)
-tyrone.left(angle)
+while count < 6:
+	tyrone.forward(length)
+	tyrone.left(angle)
+	count = count + 1
